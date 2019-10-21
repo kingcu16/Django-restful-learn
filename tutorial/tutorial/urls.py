1c6d24f43aae3c07c79d6b8f38a2b65ced0cda4b
WT <kingcu16@163.com>
2019-10-21 21:29:16 +0800

    update

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -24,11 +24,7 @@ router.register(r'groups', views.GroupViewSet)
 router.register(r'3dmodel', views.Oblique3DmodelViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
-
 urlpatterns = [
-    path('', include('snippets.urls'))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
